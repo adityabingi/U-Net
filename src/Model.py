5552d4861bffce98df14fe224a1af45a3e6e45b7
adityabingi <adityabingi786@gmail.com>
2019-11-01 19:39:46 +0530

    minor changes print statmnts removed

--- src/Model.py
+++ src/Model.py
@@ -41,17 +41,11 @@ def conv_block(inputs, filters, training, data_format):
 	"""conv_block for both encoder and decoder"""
 
 	inputs = conv_layer(inputs,filters=filters, kernel_size = 3, strides=(1,1), data_format=data_format)
-
 	inputs = batch_norm(inputs, training, data_format)
-
 	inputs = tf.nn.relu(inputs)
-
 	inputs = conv_layer(inputs, filters=filters, kernel_size=3,  strides=(1,1), data_format=data_format)
-
 	inputs = batch_norm(inputs, training, data_format)
-
 	inputs = tf.nn.relu(inputs)
-
 	return inputs
 
 def downsample(inputs, data_format):
@@ -76,9 +70,7 @@ def encoder_block(inputs, filters, training, data_format):
 	""" single encoder block"""
 
 	encoder = conv_block(inputs, filters, training, data_format)
-
 	encoder_pool = downsample(encoder, data_format)
-
 	return encoder_pool, encoder
 
 
@@ -87,13 +79,9 @@ def decoder_block(inputs, concat_tensor, filters, training, data_format):
 	""" single decoder block"""
 
 	decoder = upsample(inputs, filters, data_format)
-
 	decoder = tf.concat([concat_tensor, decoder], axis = -1)
-
 	decoder = batch_norm(decoder, training, data_format)
-
 	decoder = tf.nn.relu(decoder)
-
 	decoder = conv_block(decoder, filters, training, data_format)
 
 	return decoder
@@ -112,14 +100,9 @@ class Model:
 			self.train_dataset = train_dataset.prefetch(prefetch_batch_buffer)
 
 		self.val_dataset = val_dataset.prefetch(prefetch_batch_buffer)
-		
 		self.training  = tf.placeholder(tf.bool, shape=[])
-		
-
 		self.snapID = 0 
 		self.no_of_filters = [32, 64, 128, 256, 512, 1024]
-
-
 		# A reinitializable iterator is defined by its structure. We could use the
 		# `output_types` and `output_shapes` properties of either `training_dataset`
 		# or `validation_dataset` here, because they are compatible.
@@ -129,130 +112,55 @@ class Model:
 	 				self.val_dataset.output_shapes)
 
 		next_element = iterator.get_next()
-
 		self.imgBatch, self.labels = next_element
-
 
 		# You can use feedable iterators with a variety of different kinds of iterator
 		# (such as one-shot and initializable iterators).
-
 
 		if(train_dataset):
 			self.training_iterator = self.train_dataset.make_one_shot_iterator()
 
 		self.validation_iterator = self.val_dataset.make_initializable_iterator()
-
-
 		self.preds = self.u_net_graph(self.imgBatch)
-
 		self.loss = self.loss_function()
-
 		tf.summary.scalar('loss', self.loss)
-
-
-		#self.correct_pred = tf.equal(tf.argmax(self.preds, -1), tf.argmax(self.labels, -1))
-		#self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
-
-		#tf.summary.scalar('accuarcy', self.accuracy)
-
 		self.learningRate = tf.placeholder(tf.float32, shape=[])
 
 		update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 		with tf.control_dependencies(update_ops):
 			self.optimizer = tf.train.AdamOptimizer(self.learningRate).minimize(self.loss)
-
 		self.merged = tf.summary.merge_all()
-
 		(self.sess, self.saver) = self.setupTF()
-
-		self.train_writer = tf.summary.FileWriter(_SUMMARIES_DIR + '/train',
-                                      self.sess.graph)
+		self.train_writer = tf.summary.FileWriter(_SUMMARIES_DIR + '/train',                                      self.sess.graph)
 		self.test_writer = tf.summary.FileWriter(_SUMMARIES_DIR + '/test')
-
 
 
 	def u_net_graph(self, inputs):
 
 		""" tf graph for U_Net"""
-
-
 		with tf.variable_scope('U_net_model'):
 
-
-			print("Tensor into U_Net")
-			print(inputs.shape)
-
 			encoder1_pool, encoder1 = encoder_block(inputs, self.no_of_filters[0], self.training, self.data_format)
-
-			print("After applying 1st encoder and downsampling")
-			print(encoder1_pool.shape)
-
 			encoder2_pool, encoder2 = encoder_block(encoder1_pool,self.no_of_filters[1], self.training, self.data_format)
-
-			print("After applying 2nd encoder and downsampling")
-			print(encoder2_pool.shape)
-
 			encoder3_pool, encoder3 = encoder_block(encoder2_pool,self.no_of_filters[2], self.training, self.data_format)
-
-			print("After applying 3rd encoder and downsampling")
-			print(encoder3_pool.shape)
-
 			encoder4_pool, encoder4 = encoder_block(encoder3_pool,self.no_of_filters[3], self.training, self.data_format)
-
-			print("After applying 4th encoder and downsampling")
-			print(encoder4_pool.shape)
-
-
 			encoder5_pool, encoder5 = encoder_block(encoder4_pool,self.no_of_filters[4], self.training, self.data_format)
-
-			print("After applying 5th encoder and downsampling")
-			print(encoder5_pool.shape)
-
 
 			center = conv_block(encoder5_pool, self.no_of_filters[5], self.training, self.data_format)
 
-			print("After center convolutional block")
-			print(center.shape)
-
-
 			decoder5 = decoder_block(center, encoder5, self.no_of_filters[4], self.training, self.data_format )
-
-			print("After applying 1st decoder and upsampling")
-			print(decoder5.shape)
-
 			decoder4 = decoder_block(decoder5, encoder4, self.no_of_filters[3], self.training, self.data_format)
-
-			print("After applying 2nd decoder and upsampling")
-			print(decoder4.shape)
-
 			decoder3 = decoder_block(decoder4, encoder3, self.no_of_filters[2], self.training, self.data_format)
-
-			print("After applying 3rd decoder and upsampling")
-			print(decoder3.shape)
-
 			decoder2 = decoder_block(decoder3, encoder2, self.no_of_filters[1], self.training, self.data_format)
-
-			print("After applying 4th decoder and upsampling")
-			print(decoder2.shape)
-
 			decoder1 = decoder_block(decoder2, encoder1, self.no_of_filters[0], self.training, self.data_format)
-
-			print("After applying 5th decoder and upsampling")
-			print(decoder1.shape)
 
 			outputs = conv_layer(decoder1, 1, 1, (1,1), self.data_format, activation='sigmoid')
 
-			print(outputs.shape)
-
 			return outputs
 
-
 	def loss_function(self):
-
 		""" dice loss function and combined dice & binary cross entropy loss function;
 		    any one of them can be used"""
-
-
 		def dice_coeff(y_true, y_pred):
 			smooth = 1.
 			# Flatten
@@ -267,25 +175,19 @@ class Model:
 			return loss
 
 		def bce_dice_loss(y_true, y_pred):
-
 			loss = losses.binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
 			return loss
-
 		return dice_loss(self.labels, self.preds)
 
 
 	def setupTF(self):
-
-		"""initialize TF"""
-		
+		"""initialize TF"""	
 		config = tf.ConfigProto()
 		config.gpu_options.allow_growth = True
 		sess=tf.Session(config = config) # TF session
-
 		saver = tf.train.Saver(max_to_keep=3) # saver saves model to file
 		modelDir = '../model/'
 		latestSnapshot = tf.train.latest_checkpoint(modelDir) # is there a saved model?
-
 		# if model must be restored (for inference), there must be a snapshot
 		if self.mustRestore and not latestSnapshot:
 			raise Exception('No saved model found in: ' + modelDir)
@@ -297,12 +199,10 @@ class Model:
 		else:
 			print('Init with new values')
 			sess.run(tf.global_variables_initializer())
-
 		return (sess,saver)
 
 
 	def train(self, train_batches_per_epoch, no_of_val_batches, FilePaths):
-
 		""" training loop for model"""
 
 		epoch = 0
@@ -311,89 +211,56 @@ class Model:
 		noImprovementSince = 0
 		earlyStopping = 4
 
-
-		training_handle = self.sess.run(self.training_iterator.string_handle())
-		          
+		training_handle = self.sess.run(self.training_iterator.string_handle())          
 		validation_handle = self.sess.run(self.validation_iterator.string_handle())
-
-		
+	
 		while True:
-
 			epoch+=1
-
 			for i in range(train_batches_per_epoch):
 
 				print("Epoch :", epoch)
 				print("Training")
 				print("_"*20)
-
 				rate = 0.001 if epoch < 8  else 0.0001
-
 				_, summary,lossval = self.sess.run([self.optimizer, self.merged, self.loss], \
 					feed_dict={self.learningRate : rate, 
 					self.training : True, self.handle: training_handle})
-
 				self.train_writer.add_summary(summary, (i + (train_batches_per_epoch *(epoch-1))))
-
 				print('Batch:', i,'/', train_batches_per_epoch, 'Loss:', lossval)
-
 				batchesTrained +=1
-
-
 			mean_dice_loss = 0
 			self.sess.run(self.validation_iterator.initializer)
-
 			for i in range(no_of_val_batches):
-
-
 				print("Validating")
 				print("_"*20)
-
 				summary, lossval = self.sess.run([self.merged, self.loss],\
 				 feed_dict={ self.training : False, 
 					self.handle: validation_handle})
-
 				self.test_writer.add_summary(summary, train_batches_per_epoch*epoch )
-
 				print('Batch:', i,'/', no_of_val_batches, 'Loss:', lossval)
-
 				mean_dice_loss+= lossval
 
 			mean_dice_loss = mean_dice_loss/no_of_val_batches
-
 			print('Validation error rate after %d epochs: %f%%' % (epoch, mean_dice_loss*100.0))
 
 			if(mean_dice_loss<best_error_rate):
-
 				print("Error rate improved")
-
-
 				best_error_rate = mean_dice_loss
-
 				self.save()
-
 				with open(FilePaths.fnAccuracy, 'w') as file:
 					file.write('Validation error rate of saved model: %f%%' % (best_error_rate*100.0))
-
 			else:
-
 				print('Error rate not improved')
 				noImprovementSince += 1
-
 				# stop training if no more improvement in the last x epochs
 			if noImprovementSince >= earlyStopping:
 				print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
 				break
 
-
-
 	"""def evaluate(self):
 
-
 		validation_handle = sess.run(validation_iterator.string_handle())
-
 		mean_dice_loss = 0
-
 		while True:
 
 			try:
@@ -414,15 +281,12 @@ class Model:
 		validation_handle = self.sess.run(self.validation_iterator.string_handle())
 		self.sess.run(self.validation_iterator.initializer)
 		while True:
-
 			try:
 				preds = self.sess.run([self.preds],feed_dict={ self.training : False, \
 					self.handle: validation_handle})
 				print("predicion of image successful")
 				print(len(preds), len(preds[0]), len(preds[0][0]), len(preds[0][0][0]))
-
 				break
-
 			except tf.errors.OutOfRangeError:
 				break
 		#preds = preds.eval()
@@ -432,4 +296,3 @@ class Model:
 		"""save model to file"""
 		self.snapID += 1
 		self.saver.save(self.sess, '../model/snapshot', global_step=self.snapID)
-
--- src/main.py
+++ src/main.py
@@ -55,26 +55,18 @@ def preds_to_img(pred, actual_img, fname):
 	pred = pred[:,:]*scale
 	#pred = pred.astype(int)
 	pred = np.reshape(pred,(_IMG_SHAPE[0],_IMG_SHAPE[1],1))
-
 	cv2.imwrite(os.path.join(FilePaths.fnResults, "{}.jpg".format(fname)), actual_img)
-
-
 	cv2.imwrite(os.path.join(FilePaths.fnResults, "{}_result.jpg".format(fname)), pred)
 
 
 def main():
 	print("Inside main")
-	
 	# optional command line args
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--train", help="train the NN", action="store_true")
-
 	#parser.add_argument("--validate", help="validate the NN", action="store_true")
-	
 	parser.add_argument("--predict",nargs=1)
 	args = parser.parse_args()
-
-
 	if args.train:
 	# load training data, create TF model
 
@@ -100,38 +92,24 @@ def main():
 		model = Model(val_dataset =val_ds, train_dataset=train_ds, mustRestore = False)
 
 		model.train(train_batches_per_epoch, no_of_val_batches, FilePaths)
-
 	#elif args.validate:
-
 		#model = Model(val_dataset =val_ds, mustRestore = False)
 		#model.validate()
-
 	# infer on test image
 	elif args.predict:
-
 	# We pass test_img as dummy label to maintain dataset structure
-
 		x_val_filenames, y_val_filenames = [args.predict[0]]*32, [args.predict[0]]*32
-
 		val_ds = get_baseline_dataset(x_val_filenames,
 			y_val_filenames, 
 			batch_size=_BATCH_SIZE,
 			preproc_fn= preprocess_function(train=False),
 			threads=1)
-
 		print(open(FilePaths.fnAccuracy).read())
-
 		model = Model(val_dataset =val_ds, mustRestore = True)
 		prediction = model.infer()
-
 		fname = args.predict[0].split('/')[-1].split('.')[0]
-
 		test_img = cv2.imread(args.predict[0])
-
 		test_img = cv2.resize(test_img, (_IMG_SHAPE[0], _IMG_SHAPE[1]))
-
 		preds_to_img(prediction, test_img, fname)
-
-
 if __name__ == '__main__':
 	main()
